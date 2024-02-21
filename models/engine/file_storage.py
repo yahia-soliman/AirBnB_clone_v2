a68ebe9f238ae5d386cb9a981b320ad98823cbd6
Yahia <yn7599@gmail.com>
2024-02-21 22:39:49 +0200

    clean code rollback (it's not working 😅)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,7 +29,7 @@ class FileStorage:
         """delete obj from __objects if it’s inside"""
         if obj is None:
             return
-        key = obj.__name__ + '.' + obj.id
+        key = obj.to_dict()['__class__'] + '.' + obj.id: obj
         del FileStorage.__objects[key]
 
 
