1c9d1c7c5d8cb9d4e249946a4b114e4b31f62623
Yahia <yn7599@gmail.com>
2024-02-21 21:32:53 +0200

    test unit for console.py

---
+++ tests/test_console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A module for testing a module that needs some tests"""
+from io import StringIO 
+from unittest import TestCase 
+from unittest.mock import patch
+from console import HBNBCommand
+from models import storage
+
+
+class test_HBNBCommand(TestCase):
+    """Testing suite for the console"""
+    def test_create(self):
+        """testing the create method"""
+        with patch('sys.stdout', new = StringIO()) as fake_out: 
+            HBNBCommand().onecmd('create Place name="Awl_Abbas" max_guest=5')
+            id = fake_out.getvalue()[:-1]
+            place = storage._FileStorage__objects[f'Place.{id}']
+            self.assertEqual(place.name, "Awl Abbas")
+            self.assertEqual(place.max_guest, 5)
