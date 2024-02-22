d0d1154a8da91003b460f5bec1d325cb3e6a0e5c
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-22 03:20:59 +0200

    Task 8 - Update nullable in some instances

--- models/place.py
+++ models/place.py
@@ -14,13 +14,13 @@ class Place(BaseModel, Base):
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
 
-    description = Column(String(1024), )
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
 
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
     reviews = relationship('Review', cascade='all, delete', backref='place')
