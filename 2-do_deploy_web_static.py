23d62fbace86d60c739dd97e294ce6ba143675e5
Yahia <yn7599@gmail.com>
2024-03-07 21:39:34 +0200

    runs once

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """deploy a packaged web_static archive"""
 from fabric.api import put, run, env
+from os.path import exists
 
 env.hosts = ['54.210.126.177', '18.208.120.189']
 
 
 def do_deploy(archive_path):
     """distribute an archive to web servers"""
+    if not (archive_path and exists(archive_path)):
+        return False
     path = put(archive_path, '/tmp/')
     if path.failed:
         return False
     outdir = '/data/web_static/releases/'
-    if run(f'tar -xzf {path[0]} --directory {outdir}').failed:
-        return False
-    new_name = path[0].split('/')[-1].split('.')[0]
     link = "/data/web_static/current"
-    if run(f'rm -rf {outdir + new_name} {path[0]} {link}').failed:
-        return False
-    if run('mv ' + outdir + '{web_static,' + new_name + '}').failed:
-        return False
-    return run(f'ln -sf {outdir + new_name} {link}').succeeded
+    outdir += path[0].split('/')[-1].split('.')[0]
+    run(f'mkdir -p {outdir}')
+    run(f'tar -xzf {path[0]} --directory {outdir}')
+    run(f'rm -rf {path[0]}')
+    run(f'rm -rf {link}')
+    run(f'ln -sf {outdir} {link}')
+    run('mv ' + outdir + '/web_static/* ' + outdir)
+    return True
