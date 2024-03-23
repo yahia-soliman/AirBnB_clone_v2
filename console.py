90667bbd5d22d815d8d3d6257c8465218a1464dd
Yahia <yn7599@gmail.com>
2024-03-24 01:28:20 +0200

    flask + mysql

--- console.py
+++ console.py
@@ -213,7 +213,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -234,7 +234,7 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
                 return
             else:
-               all = storage.all(HBNBCommand.classes[cls])
+                all = storage.all(HBNBCommand.classes[cls])
         else:
             all = storage.all()
 
@@ -347,6 +347,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
