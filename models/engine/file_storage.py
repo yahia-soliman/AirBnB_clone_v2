6b362d5319b08c66636b0fad901642435690331b
Yahia <yn7599@gmail.com>
2024-03-24 15:32:12 +0200

    I'm done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        self.close()
         final = {}
         if cls is None:
             return FileStorage.__objects
